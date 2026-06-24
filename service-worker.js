cbaa5cd46c41cb6d65042d0f4a882abc4e4c93b6
aerorecto <167107220+aerorecto@users.noreply.github.com>
2026-06-24 19:12:08 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,13 @@
-const CACHE_NAME = "flight-timestamp-v5";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
+const CACHE_NAME = "flight-timestamp-zulu-v6";
+const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request).catch(() => caches.match("./index.html"));
-    })
-  );
+  event.respondWith(caches.match(event.request).then(cachedResponse => cachedResponse || fetch(event.request).catch(() => caches.match("./index.html"))));
 });
