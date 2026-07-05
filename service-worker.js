637e8bd47a562cb16d8ae57dda7fb51d20a0e62f
aero.code <167107220+aerorecto@users.noreply.github.com>
2026-07-05 18:26:28 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,6 @@
-const CACHE_NAME = "aero-timestamp-v7";
+const CACHE_NAME = "aero-timestamp-v8";
 const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cachedResponse => cachedResponse || fetch(event.request).catch(() => caches.match("./index.html"))));
-});
+self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))); self.skipWaiting(); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
+self.addEventListener("message", event => { if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting(); });
+self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(response => { const clone = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)); return response; }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))); });
