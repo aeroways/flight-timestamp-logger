c4389cd16362cd013b8907c3391bd124a45fbc69
aero.code <167107220+aeroways@users.noreply.github.com>
2026-07-06 23:52:15 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aero-timestamp-v9";
+const CACHE_NAME = "aero-timestamp-v8";
 const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
